76b19c8cc1ad69d71e2d2cf3a62903362d11f5a7
vjeedi <60668823+vjeedi@users.noreply.github.com>
2024-02-13 08:50:03 -0500

    added favicon

--- code/admin/pages/02_Explore_Data.py
+++ code/admin/pages/02_Explore_Data.py
@@ -7,11 +7,14 @@ import pandas as pd
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utilities.helpers.AzureSearchHelper import AzureSearchHelper
+from getImageFromURL import getImageFromURL
 from dotenv import load_dotenv
 load_dotenv()
 
 logger = logging.getLogger('azure.core.pipeline.policies.http_logging_policy').setLevel(logging.WARNING)
-st.set_page_config(page_title="Explore Data", page_icon=os.path.join('images','favicon.ico'), layout="wide", menu_items=None)
+# st.set_page_config(page_title="Explore Data", page_icon=os.path.join('images','favicon.ico'), layout="wide", menu_items=None)
+st.set_page_config(page_title="Explore Data", page_icon=getImageFromURL(os.getenv('FAVICON_URL')), layout="wide", menu_items=None)
+
 mod_page_style = """
             <style>
             #MainMenu {visibility: hidden;}
